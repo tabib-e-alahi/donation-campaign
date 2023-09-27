166186d7cb86ee15bad745d16d48c9bb801f4241
tabib-e-alahi <etabib.alahi@gmail.com>
2023-09-27 19:32:01 +0600

    cards data title and img changes and some styles  added

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -18,7 +18,7 @@ const Cards = ({ card }) => {
    
       <div className="card" onClick={() => handleDonationDetails(id)}>
         <figure className="">
-          <img className="w-full" src={img} alt="" />
+          <img className="w-full h-48" src={img} alt="" />
         </figure>
         <div style={{backgroundColor: card_bg}} className="card-body  p-4 rounded-b-lg">
           <p style={{backgroundColor: category_bg , color:text_button_bg}} className="px-3 py-0.5 w-max rounded text-sm font-medium">{category_name}</p>
